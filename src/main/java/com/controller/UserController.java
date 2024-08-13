0885865cbf8f2a1874f7e890c59d047ddbdbebde
ParamDholakiya <paramdholakia@gmail.com>
2024-08-13 16:34:48 +0530

    Added explanatory Comments

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,23 +1,40 @@
 package com.controller;
 
+// Import statements for Spring framework annotations
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+// Importing the UserEntity class, which likely represents a user in the system
 import com.entity.UserEntity;
 
-@Controller
+@Controller  // Marks this class as a Spring MVC controller
 public class UserController {
+
+    // Method to handle GET requests for the "/newUser" URL
+    // This method returns the name of the view (JSP page) to be rendered
     @GetMapping("/newUser")
     public String newUser() {
-        return "NewUser";  // This should match the JSP file name
+        // Returns the name of the JSP file that should be displayed
+        // The view resolver will look for a file named "NewUser.jsp"
+        return "NewUser";
     }
-	
-	@PostMapping("/Saveuser")
-	public String saveUser(UserEntity user) {
-		System.out.println("First Name: " + user.getFirstName());
-		System.out.println("Email: " + user.getEmail());
-		System.out.println("Password: " + user.getPassword());
-		return "NewUser";
-	}
+    
+    // Method to handle POST requests for the "/Saveuser" URL
+    // This method receives a UserEntity object, which is populated with data from the form submission
+    @PostMapping("/Saveuser")
+    public String saveUser(UserEntity user) {
+        // Print the user's first name to the console for debugging purposes
+        System.out.println("First Name: " + user.getFirstName());
+
+        // Print the user's email to the console for debugging purposes
+        System.out.println("Email: " + user.getEmail());
+
+        // Print the user's password to the console for debugging purposes
+        System.out.println("Password: " + user.getPassword());
+
+        // After processing, return the name of the JSP file to be displayed
+        // This will show the "NewUser.jsp" page again
+        return "NewUser";
+    }
 }
